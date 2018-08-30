eec225ac5e5acb26dc6b57e9774ace81ee5e5d40
clifford <clifford@noukod.com>
2018-08-30 14:05:28 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,9 +1,4 @@
 # code the #greeting method here!
 def greeting(name)
-puts "Hi! Welcome to the wonderful world of Ruby programming."
-
-puts "Please enter your name so that we can greet you more personally:"
-
-name = gets.strip
-greeting(name)
+puts "hello #{name}. I am happy to mmeet you."
 end
